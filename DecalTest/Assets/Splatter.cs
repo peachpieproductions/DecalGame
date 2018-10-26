63740be67ff39717d06f732409a9134bc44b1577
peachpieproductions <benjames.im@gmail.com>
2018-10-26 15:58:24 -0400

    Added new splat graphics

--- DecalTest/Assets/Splatter.cs
+++ DecalTest/Assets/Splatter.cs
@@ -7,15 +7,20 @@ public class Splatter : MonoBehaviour {
 
     public float size = .5f;
     public float fullSize;
+    public Texture2D[] splatTextures;
 
     private void Start() {
         fullSize = transform.localScale.x;
-        fullSize += Random.Range(0, .5f);
+        fullSize += Random.Range(0, .3f);
+        ProjectionRenderer ren = GetComponent<ProjectionRenderer>();
+        var splatIndex = Random.Range(0, 4);
+        ren.Offset = new Vector2(.25f * splatIndex, 0);
+        ren.UpdateProperties();
     }
 
     private void Update() {
         if (size < 1) {
-            size = Mathf.Min(1, size + .12f);
+            size = Mathf.Min(1, size + Time.deltaTime * 15f);
             transform.localScale = Vector3.one * size * fullSize;
         } else {
             Destroy(this);
